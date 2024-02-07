34ec4c686feb0d902f737705ec8ff6d5c8d310a2
princebtr <princekumar07014@gmail.com>
2024-02-07 23:46:23 +0530

    solved a question on codeforces named Drinks 200b

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
-import static codeforces.Soroban363.soroban;
-import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        soroban(num);
+
     }
 }
---
+++ src/main/java/codeforces/Drinks200.java
@@ -0,0 +1,23 @@
+package codeforces;
+
+public class Drinks200 {
+    public static float drinkcalculator(int[] drink) {
+        int len = drink.length, sum = 0, total = len * 100;
+        for (int i = 0; i < len; i++) {
+            sum += drink[i];
+        }
+        return (float) sum / len;
+    }
+}
+
+/* Main
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] drink = new int[len];
+        for(int i=0;i<len;i++){
+            drink[i] = sc.nextInt();
+        }
+        System.out.println(drinkcalculator(drink));
+    }
+/*
