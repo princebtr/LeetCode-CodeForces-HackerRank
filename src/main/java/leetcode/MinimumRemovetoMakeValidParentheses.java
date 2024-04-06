f45fa8f232a3bbd4706b3d480c0a68e7fa7b6dd6
princebtr <princekumar07014@gmail.com>
2024-04-06 23:07:48 +0530

    solved a question on leetcode 1249. Minimum Remove to Make Valid Parentheses

---
+++ src/main/java/leetcode/MinimumRemovetoMakeValidParentheses.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+public class MinimumRemovetoMakeValidParentheses {
+    public String minRemoveToMakeValid(String s) {
+        char[] ch = s.toCharArray();
+        int count = 0;
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == '(') {
+                count++;
+            } else if (ch[i] == ')') {
+                if (count > 0) {
+                    count--;
+                } else {
+                    ch[i] = 0;
+                }
+            }
+        }
+        count = 0;
+        for (int i = ch.length - 1; i >= 0; i--) {
+            if (ch[i] == ')') {
+                count++;
+            } else if (ch[i] == '(') {
+                if (count > 0) {
+                    count--;
+                } else {
+                    ch[i] = 0;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : ch) {
+            if (c != 0) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
